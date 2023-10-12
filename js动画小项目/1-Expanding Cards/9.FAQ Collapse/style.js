0f0f331de25b4651b14f9a1959899b36f522c25a
easonmia <141611456+easonmia@users.noreply.github.com>
2023-10-13 00:19:05 +1300

    Add files via upload

---
+++ js动画小项目/1-Expanding Cards/9.FAQ Collapse/style.js
@@ -0,0 +1,8 @@
+window.onload = function(){
+    const toggles = document.querySelectorAll('.faq-toggle')
+    toggles.forEach(toggle=>{
+        toggle.addEventListener('click',()=>{
+            toggle.parentNode.classList.toggle('active')
+        })
+    })
+}
